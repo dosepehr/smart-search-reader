2543ba8c52bf5d92022d865724c0fa561beb3c01
dosepehr <dosepehr@gmail.com>
2025-09-14 22:12:00 +0330

    add logo image to entries

--- next.config.ts
+++ next.config.ts
@@ -3,34 +3,37 @@ import withPWA from 'next-pwa';
 const isDev = process.env.NODE_ENV === 'development';
 
 export default withPWA({
-  dest: 'public',
-  disable: isDev,
-  register: true,
-  skipWaiting: true,
-  // Don't use automatic precaching of _next files
-  buildExcludes: [/app-build-manifest\.json$/, /react-loadable-manifest\.json$/],
-  fallbacks: {
-    document: '/offline',
-    image: '/offline',
-    font: '/offline',
-    audio: '/offline',
-    video: '/offline',
-  },
-  runtimeCaching: [
-    {
-      urlPattern: /^\/$/,
-      handler: 'NetworkFirst',
-      options: { cacheName: 'pages-cache' },
+    dest: 'public',
+    disable: isDev,
+    register: true,
+    skipWaiting: true,
+    buildExcludes: [
+        /app-build-manifest\.json$/,
+        /react-loadable-manifest\.json$/,
+    ],
+    fallbacks: {
+        document: '/offline',
+        image: '/offline',
+        font: '/offline',
+        audio: '/offline',
+        video: '/offline',
     },
-    {
-      urlPattern: /^\/offline$/,
-      handler: 'CacheFirst',
-      options: { cacheName: 'pages-cache' },
-    },
-    {
-      urlPattern: /.+\.(?:png|jpg|jpeg|gif|webp|svg)$/i,
-      handler: 'CacheFirst',
-      options: { cacheName: 'images-cache' },
-    },
-  ],
+    additionalManifestEntries: [{ url: '/images/logo.jpg', revision: null }],
+    runtimeCaching: [
+        {
+            urlPattern: /^\/$/,
+            handler: 'NetworkFirst',
+            options: { cacheName: 'pages-cache' },
+        },
+        {
+            urlPattern: /^\/offline$/,
+            handler: 'CacheFirst',
+            options: { cacheName: 'pages-cache' },
+        },
+        {
+            urlPattern: /.+\.(?:png|jpg|jpeg|gif|webp|svg)$/i,
+            handler: 'CacheFirst',
+            options: { cacheName: 'images-cache' },
+        },
+    ],
 });
