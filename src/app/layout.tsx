4bd4eb14190e8606fa7b64745d0aa25f74dd7b6a
dosepehr <dosepehr@gmail.com>
2025-09-14 19:34:19 +0330

    edit pwa configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,14 +29,14 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <head>
-                <meta name='application-name' content='PWA App' />
+                <meta name='application-name' content='Smart Search Reader' />
                 <meta name='apple-mobile-web-app-capable' content='yes' />
                 <meta
                     name='apple-mobile-web-app-status-bar-style'
                     content='default'
                 />
-                <meta name='apple-mobile-web-app-title' content='PWA App' />
-                <meta name='description' content='Best PWA App in the world' />
+                <meta name='apple-mobile-web-app-title' content='Smart Search Reader' />
+                <meta name='description' content='Best Smart Search Reader in the world' />
                 <meta name='format-detection' content='telephone=no' />
                 <meta name='mobile-web-app-capable' content='yes' />
                 <meta
@@ -45,7 +45,7 @@ export default function RootLayout({
                 />
                 <meta name='msapplication-TileColor' content='#2B5797' />
                 <meta name='msapplication-tap-highlight' content='no' />
-                <meta name='theme-color' content='#000000' />
+                <meta name='theme-color' content='#374151' />
 
                 <link
                     rel='icon'
@@ -112,29 +112,29 @@ export default function RootLayout({
                 <link rel='shortcut icon' href='/favicon.ico' />
 
                 <meta name='twitter:card' content='summary' />
-                <meta name='twitter:url' content='https://yourdomain.com' />
-                <meta name='twitter:title' content='PWA App' />
+                <meta name='twitter:url' content='https://smart-search-reader-sable.vercel.app' />
+                <meta name='twitter:title' content='Smart Search Reader' />
                 <meta
                     name='twitter:description'
-                    content='Best PWA App in the world'
+                    content='Best Smart Search Reader in the world'
                 />
                 <meta
                     name='twitter:image'
-                    content='https://yourdomain.com/icons/icon-192x192.png'
+                    content='https://smart-search-reader-sable.vercel.app/icons/icon-192x192.png'
                 />
                 <meta name='twitter:creator' content='@DavidWShadow' />
 
                 <meta property='og:type' content='website' />
-                <meta property='og:title' content='PWA App' />
+                <meta property='og:title' content='Smart Search Reader' />
                 <meta
                     property='og:description'
-                    content='Best PWA App in the world'
+                    content='Best Smart Search Reader in the world'
                 />
-                <meta property='og:site_name' content='PWA App' />
-                <meta property='og:url' content='https://yourdomain.com' />
+                <meta property='og:site_name' content='Smart Search Reader' />
+                <meta property='og:url' content='https://smart-search-reader-sable.vercel.app' />
                 <meta
                     property='og:image'
-                    content='https://yourdomain.com/icons/icon-512x512.png'
+                    content='https://smart-search-reader-sable.vercel.app/icons/icon-512x512.png'
                 />
             </head>
 
